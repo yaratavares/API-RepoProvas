3445443197cd8b326c3181abd8292584ad86cd94
Yara Tavares <yaracriistavares@gmail.com>
2022-04-20 11:55:08 -0300

    docs: server is running, ready to code

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+export default app;
