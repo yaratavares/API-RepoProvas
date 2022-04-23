d41c2ae6609e6974ab9058503db68295be212cae
Yara Tavares <yaracriistavares@gmail.com>
2022-04-23 19:50:38 -0300

    docs: create database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
+import router from "./router/index.routes.js";
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router);
 
 export default app;
