25d3e7d0abea74a32e7bea60d9659e536faadaa6
Yara Tavares <yaracriistavares@gmail.com>
2022-04-25 15:37:29 -0300

    fix: change port for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import "./setup.ts";
 
-const port = +process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
